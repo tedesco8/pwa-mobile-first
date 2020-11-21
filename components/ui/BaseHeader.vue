deb95a27ff0b4f00e402dc316d119b2880a36216
tedesco8 <tedesco8@hotmail.com>
2020-11-20 23:44:38 -0300

    feat: creando componente header

---
+++ components/ui/BaseHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header
+    class="flex items-center justify-between p-4 dark:text-white dark:bg-blue-main-800"
+  >
+    <nuxt-link class="flex items-center" to="/">
+      <img
+        src="../../assets/icons/inglesLogoGraduado.png"
+        alt=""
+        class="w-6 mr-3"
+      />
+      <h1 class="font-bold text-base">English - Desde Cero</h1>
+    </nuxt-link>
+    <nuxt-link class="hidden md:block" to="/profile">
+      <img
+        class="w-10 rounded-full"
+        src="../../assets/images/defaultImage.jpg"
+        alt=""
+      />
+    </nuxt-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'BaseHeader',
+}
+</script>
